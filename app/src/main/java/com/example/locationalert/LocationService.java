5746232ae5b461b0a3dceac04c9b512c383c24ba
Sina Honarvar <sina.ho79@gmail.com>
2023-04-13 00:29:59 +0330

    V2-app works in foreground with Services

---
+++ app/src/main/java/com/example/locationalert/LocationService.java
@@ -0,0 +1,135 @@
+package com.example.locationalert;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+public class LocationService extends Service {
+//    public LocationService() {
+//
+//    }
+
+    private final int LOCATION_REFRESH_TIME = 5000; // 5 seconds to update
+    private final int LOCATION_REFRESH_DISTANCE = 1; // 1 meters to update
+
+    private void createNotificationChannel() {
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("0x1", name, importance);
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setShowBadge(true);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private NotificationManagerCompat notificationManager;
+    private NotificationCompat.Builder redAreaBuilder;
+    private NotificationCompat.Builder blueAreaBuilder;
+    private int notificationId = 0;
+
+
+    public final LocationListener mLocationListener = new LocationListener() {
+        private long lastColor;
+
+
+        @Override
+        public void onLocationChanged(final Location location) {
+            long color = LocationDetector.getLocationColor(location.getLongitude(), location.getLatitude());
+            Log.i("Color", Long.toString(color, 10));
+            if (color != lastColor) {
+                if (color == Color.BLUE) {
+                    blueAreaBuilder.setWhen(System.currentTimeMillis());
+                    notificationManager.notify(notificationId++, blueAreaBuilder.build());
+                } else if (color == Color.RED) {
+                    redAreaBuilder.setWhen(System.currentTimeMillis());
+                    notificationManager.notify(notificationId++, redAreaBuilder.build());
+                }
+                lastColor = color;
+            }
+        }
+    };
+
+
+    @Override
+    public void onCreate() {
+        Log.i("SERVICE_LC", "Created");
+        notificationManager = NotificationManagerCompat.from(this);
+        LocationDetector.InitSingleton(this, 46.326797, 38.055580, 46.328095, 38.055017);
+        redAreaBuilder = new NotificationCompat.Builder(this, "0x1")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("RED AREA!")
+                .setContentText("You have just entered the red area.")
+                .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND | Notification.FLAG_SHOW_LIGHTS)
+                .setAutoCancel(false);
+        blueAreaBuilder = new NotificationCompat.Builder(this, "0x1")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("BLUE AREA!")
+                .setContentText("You have just entered the blue area.")
+                .setAutoCancel(true);
+        NotificationCompat.Builder staticNotifications = new NotificationCompat.Builder(this, "0x1")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(false)
+                .setContentTitle("App is running on foreground")
+                .setPriority(NotificationManager.IMPORTANCE_LOW)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setOngoing(true);
+        startForeground(9999, staticNotifications.build());
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean accessGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        if (accessGranted) {
+            mLocationManager.requestLocationUpdates(LocationManager.FUSED_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        createNotificationChannel();
+        Log.i("SERVICE_LC", "Started");
+
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("SERVICE_LC", "Destroyed");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/com/example/locationalert/MainActivity.java
+++ app/src/main/java/com/example/locationalert/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -34,73 +35,32 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final int LOCATION_REFRESH_TIME = 5000; // 5 seconds to update
-    private final int LOCATION_REFRESH_DISTANCE = 1; // 1 meters to update
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
-    private void createNotificationChannel() {
-
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("0x1", name, importance);
-            channel.setDescription(description);
-            channel.enableLights(true);
-            channel.enableVibration(true);
-            channel.setShowBadge(true);
-
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private NotificationManagerCompat notificationManager;
-    private NotificationCompat.Builder redAreaBuilder;
-    private NotificationCompat.Builder blueAreaBuilder;
-    private int notificationId = 0;
-    private long lastColor = Color.WHITE;
-    public final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            long color = LocationDetector.getLocationColor(location.getLongitude(), location.getLatitude());
-            TextView mainText = (TextView) findViewById(R.id.textview_first);
-            mainText.setText(String.format("%s : %s \ncolor is : %s\nis mock %s, %s", location.getLatitude(), location.getLongitude(), color == Color.WHITE ? "White" : color == Color.BLUE ? "Blue" : "RED ", location.isFromMockProvider(), location.getProvider()));
-            Log.i("Color", Long.toString(color, 10));
-            if (color != lastColor) {
-                if (color == Color.BLUE) {
-                    notificationManager.notify(notificationId++, blueAreaBuilder.build());
-                } else if (color == Color.RED) {
-                    notificationManager.notify(notificationId++, redAreaBuilder.build());
-                }
-                lastColor = color;
-            }
-        }
-    };
+//    private int notificationId = 0;
+//    private long lastColor = Color.WHITE;
+//    public final LocationListener mLocationListener = new LocationListener() {
+//        @Override
+//        public void onLocationChanged(final Location location) {
+//            long color = LocationDetector.getLocationColor(location.getLongitude(), location.getLatitude());
+//            TextView mainText = (TextView) findViewById(R.id.textview_first);
+//            mainText.setText(String.format("%s : %s \ncolor is : %s\nis mock %s, %s", location.getLatitude(), location.getLongitude(), color == Color.WHITE ? "White" : color == Color.BLUE ? "Blue" : "RED ", location.isFromMockProvider(), location.getProvider()));
+//            Log.i("Color", Long.toString(color, 10));
+//            if (color != lastColor) {
+//                if (color == Color.BLUE) {
+//                    notificationManager.notify(notificationId++, blueAreaBuilder.build());
+//                } else if (color == Color.RED) {
+//                    notificationManager.notify(notificationId++, redAreaBuilder.build());
+//                }
+//                lastColor = color;
+//            }
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createNotificationChannel();
-        notificationManager = NotificationManagerCompat.from(this);
-
-        redAreaBuilder = new NotificationCompat.Builder(this, "0x1")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("RED AREA!")
-                .setContentText("You have just entered the red area.")
-                .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND | Notification.FLAG_SHOW_LIGHTS)
-                .setAutoCancel(false);
-        blueAreaBuilder = new NotificationCompat.Builder(this, "0x1")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("BLUE AREA!")
-                .setContentText("You have just entered the blue area.")
-                .setAutoCancel(true);
 
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -116,10 +76,15 @@ public class MainActivity extends AppCompatActivity {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
             accessGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
         }
-        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        mLocationManager.requestLocationUpdates(LocationManager.FUSED_PROVIDER, LOCATION_REFRESH_TIME,
-                LOCATION_REFRESH_DISTANCE, mLocationListener);
+        Intent LocationServiceIntent = new Intent(this, LocationService.class);
+        Log.i("SERVICE_LC", "Before start");
 
+        startService(LocationServiceIntent);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            startForegroundService(LocationServiceIntent);
+//        } else {
+//            startService(LocationServiceIntent);
+//        }
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        LocationDetector.InitSingleton(this, 46.326797, 38.055580, 46.328095, 38.055017);
+
     }
 
     @Override
